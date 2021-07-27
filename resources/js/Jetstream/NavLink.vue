716fde17a332175172f89bf13aafed066e6cf6ed
Osiris <usir95.hp@gmail.com>
2021-07-27 17:30:06 -0500

    Navbar

--- resources/js/Jetstream/NavLink.vue
+++ resources/js/Jetstream/NavLink.vue
@@ -11,8 +11,8 @@
         computed: {
             classes() {
                 return this.active
-                            ? 'tw-inline-flex tw-items-center tw-px-1 tw-pt-1 tw-border-b-2 tw-border-indigo-400 tw-text-sm tw-font-medium tw-leading-5 tw-text-gray-900 focus:tw-outline-none focus:tw-border-indigo-700 tw-transition'
-                            : 'tw-inline-flex tw-items-center tw-px-1 tw-pt-1 tw-border-b-2 tw-border-transparent tw-text-sm tw-font-medium tw-leading-5 tw-text-gray-500 hover:tw-text-gray-700 hover:tw-border-gray-300 focus:tw-outline-none focus:tw-text-gray-700 focus:tw-border-gray-300 tw-transition'
+                            ? 'tw-bg-gray-50 tw-text-blue-500 tw-inline-block tw-border-l tw-border-t tw-border-r tw-rounded-t tw-px-4 tw-py-2 tw-mt-4 tw-font-semibold'
+                            : 'tw-inline-flex tw-items-center tw-px-1 tw-pt-4 tw-border-b-2 tw-border-transparent tw-text-sm tw-font-medium tw-leading-5 tw-text-gray-500 hover:tw-text-gray-700 hover:tw-border-gray-400 focus:tw-outline-none focus:tw-text-gray-700 focus:tw-border-gray-300 tw-transition'
             }
         }
     }
