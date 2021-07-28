bc57118d3810cfa09795e389f519b61e6ee9dbe1
Osiris <usir95.hp@gmail.com>
2021-07-28 15:17:59 -0500

    Vista Admin

--- resources/js/Pages/Menus/Admin.vue
+++ resources/js/Pages/Menus/Admin.vue
@@ -1,5 +1,21 @@
 <template>
     <app-layout>
+        <template #header>
+            <div class="tw-mt-2 tw-text-center tw-text-white tw-bg-green-600 tw-shadow-2xl tw-rounded-2xl tw-mr-16 tw-ml-16">
+                <h3 class="tw-p-2"><i class="fas fa-house-user tw-ml-3 tw-mr-3"></i>Menu Administrador</h3>
+            </div>
+        </template>
 
     </app-layout>
 </template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    import Welcome from '@/Jetstream/Welcome'
+
+    export default {
+        components: {
+            AppLayout,
+        },
+    }
+</script>
