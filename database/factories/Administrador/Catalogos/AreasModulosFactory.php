604c2ccc7521437e4df6b9d0327a4970d8087db9
Osiris <usir95.hp@gmail.com>
2021-07-29 09:42:35 -0500

    Areas modulos modelo

---
+++ database/factories/Administrador/Catalogos/AreasModulosFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Administrador\Catalogos;
+
+use App\Models\Administrador\Catalogos\AreasModulos;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AreasModulosFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AreasModulos::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
