79d644336da7fe9776ebd11d88e4b59b85124ccf
Osiris <usir95.hp@gmail.com>
2021-09-10 17:09:15 -0500

    Ruta y menu Supply

--- app/Http/Controllers/Menus/MenuSupplyController.php
+++ app/Http/Controllers/Menus/MenuSupplyController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Menus;
 
 use App\Http\Controllers\Controller;
+use App\Models\Administrador\Modulos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class MenuSupplyController extends Controller
 {
-    //
+    public function index() {
+
+        $usuario = Auth::user();
+        $modulos = Modulos::where("Area","=","8")->get();
+
+        return Inertia::render('Menus/Supply', ['usuario' => $usuario,'modulos' => $modulos]);
+    }
 }
