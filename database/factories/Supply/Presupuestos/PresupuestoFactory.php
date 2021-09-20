3bc885afaa9eb1af88e7aa55fe789662b993b19e
Osiris <usir95.hp@gmail.com>
2021-09-20 13:32:09 -0500

    Presupuestos Por Area Modulo

---
+++ database/factories/Supply/Presupuestos/PresupuestoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Supply\Presupuestos;
+
+use App\Models\Supply\Presupuestos\Presupuesto;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PresupuestoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Presupuesto::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
