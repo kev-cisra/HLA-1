db0e4ac24efdf3c02ccf67ebfce4b8abb9295db8
Osiris <usir95.hp@gmail.com>
2021-09-28 17:06:52 -0500

    Maquinas por Area

--- app/Http/Controllers/Compras/Requisiciones/MaquinasController.php
+++ app/Http/Controllers/Compras/Requisiciones/MaquinasController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers\Compras\Requisiciones;
 
 use App\Http\Controllers\Controller;
 use App\Models\Catalogos\Maquinas;
+use App\Models\Catalogos\MarcasMaquinas;
 use Illuminate\Http\Request;
 
 class MaquinasController extends Controller
 {
     public function Dropdown(Request $request){
 
-        $DeptoId = $request->input('Departamento_id');
+        $MaquinaId = $request->input('Departamento');
 
-        return $data = Maquinas::where('departamento_id', $DeptoId)->get();
+        $data = Maquinas::where('departamento_id', $MaquinaId)->get();
 
-        return response()->json(["Maquinas" => $data ]);
+        return response()->json(["Marcas" => $data ]);
     }
 }
