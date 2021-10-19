e3e029afdd30c0f02eabae6fd28ca731c62ed779
kev-cisra <87038939+kev-cisra@users.noreply.github.com>
2021-10-19 12:36:17 -0500

    calculo de operaciones

--- app/Http/Controllers/Produccion/Calculos/CalculosController.php
+++ app/Http/Controllers/Produccion/Calculos/CalculosController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Produccion\carga;
 use App\Models\Produccion\catalogos\procesos;
 use App\Models\Produccion\turnos;
+use App\Models\RecursosHumanos\Perfiles\PerfilesUsuarios;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use SebastianBergmann\Environment\Console;
 
 class CalculosController extends Controller
@@ -15,6 +17,9 @@ class CalculosController extends Controller
     //
     public function store(Request $request)
     {
+        $usuario = Auth::id();
+        $perf = PerfilesUsuarios::where('user_id','=',$usuario)
+            ->first('id');
         //pone la hora de inicio y fin para consultar
         $hIni = ' 07:00:00';
         if ($request->depa == 7 & !empty(Carbon::createFromDate($request->fecha)->isDST())) {
@@ -28,7 +33,7 @@ class CalculosController extends Controller
         $hoy = Carbon::create($request->fecha.$hIni)->format('Y-m-d H:i:s');
         $mañana = Carbon::create($request->fecha.$hIni)->addDays(1)->format('Y-m-d H:i:s');
         $semana = date('Y',strtotime($request->fecha)).'-W'.date('W',strtotime($request->fecha));
-        $fechas = ['hoy' => $hoy, 'mañana' => $mañana, 'semana' => $semana];
+        $fechas = ['fecha' => $request->fecha, 'hoy' => $hoy, 'mañana' => $mañana, 'semana' => $semana];
 
         $calcula = procesos::where('departamento_id', '=', $request->depa)
                     ->where('tipo', '=', '3')
@@ -43,92 +48,117 @@ class CalculosController extends Controller
             //dependiendo del tipo de operacion
             switch ($ope->operacion) {
                 case 'sm_d':
-                    $this->sm_d($ope, $fechas);
+                    $this->sm_d($ope, $fechas, $perf);
                     break;
                 case 'sm_dc':
-                    //$this->sm_dc($ope, $fechas);
+                    $this->sm_dc($ope, $fechas, $perf);
                     break;
                 case 'sm_t':
-                    //$this->sm_t($ope, $request->depa, $fechas);
+                    $this->sm_t($ope, $request->depa, $fechas, $perf);
                     break;
                 case 'sm_tc':
-                    //$this->sm_tc($ope, $request->depa, $fechas);
+                    $this->sm_tc($ope, $request->depa, $fechas, $perf);
                     break;
             }
         }
 
-        return 'Listo';
+        //return 'Listo';
 
-        /* return redirect()->back()
-            ->with('message', 'Post Created Successfully.'); */
+        return redirect()->back()
+            ->with('message', 'Post Created Successfully.');
     }
     /************************************** Guardado o Actualizado ************************************/
-    public function gua_act($fec, $datos, $suma, $canti){
+    public function gua_act($fec, $data){
         //consulta si existe el dato
-        echo $fec.' '.$datos.' '.$suma.' '.$canti;
+        $existe = carga::where('proceso_id', '=', $data['proceso_id'])
+            ->where('clave_id', '=', $data['clave_id'])
+            ->where('turno_id', '=', $data['turno_id'])
+            ->first();
+        if (empty($existe)) {
+            carga::create([
+                'fecha' => $fec['fecha'].' 13:00:00',
+                'semana' => $fec['semana'],
+                'partida' => $data['partida'],
+                'valor' => $data['suma'],
+                'notaPen' => '1',
+                'estatus' => '1',
+                'VerInv' => $data['cantidad'],
+                'per_carga' => $data['per_carga'],
+                'proceso_id' => $data['proceso_id'],
+                'norma' => $data['norma'],
+                'clave_id' => $data['clave_id'],
+                'turno_id' => $data['turno_id']
+            ]);
+        }else {
+            carga::find($existe->id)->update([
+                'partida' => $data['partida'],
+                'valor' => $data['suma'],
+                'VerInv' => $data['cantidad'],
+                'per_carga' => $data['per_carga'],
+                'proceso_id' => $data['proceso_id'],
+                'norma' => $data['norma'],
+                'clave_id' => $data['clave_id'],
+                'turno_id' => $data['turno_id']
+            ]);
+        }
     }
 
     /************************************** Operaciones ***********************************************/
     //operacion suma del dia
-    public function sm_d($val, $fechas){
+    public function sm_d($val, $fechas, $usuario){
         $fs = 0;
         $fc = 0;
         //recorrido de formulas
         foreach ($val->formulas as $value) {
-
             $proce_id = $value->proceso_id;
             $maq_id = $value->maq_pros_id;
             //Consulta para la suma
             $suma = carga::whereBetween('fecha', [$fechas['hoy'], $fechas['mañana']])
-            ->where(function($qm) use ($maq_id){
-                $qm ->whereNotNull('maq_pro_id')
-                    -> where('maq_pro_id', '=', $maq_id);
-            })
-            ->orWhere(function($q) use ($proce_id){
-                $q -> whereNull('maq_pro_id')
-                    ->where('proceso_id', '=', $proce_id);
-            })
-            ->sum('valor');
+                ->where(function($qm) use ($maq_id){
+                    $qm ->whereNotNull('maq_pro_id')
+                        -> where('maq_pro_id', '=', $maq_id);
+                })
+                ->orWhere(function($q) use ($proce_id){
+                    $q -> whereNull('maq_pro_id')
+                        ->where('proceso_id', '=', $proce_id);
+                })
+                ->sum('valor');
+
             //Consulta de paquetes contados
             $cuenta = carga::whereBetween('fecha', [$fechas['hoy'], $fechas['mañana']])
-            ->where(function($qm) use ($maq_id){
-                $qm ->whereNotNull('maq_pro_id')
-                    -> where('maq_pro_id', '=', $maq_id);
-            })
-            ->orWhere(function($q) use ($proce_id){
-                $q -> whereNull('maq_pro_id')
-                    ->where('proceso_id', '=', $proce_id);
-            })
-            ->count('valor');
+                ->where(function($qm) use ($maq_id){
+                    $qm ->whereNotNull('maq_pro_id')
+                        -> where('maq_pro_id', '=', $maq_id);
+                })
+                ->orWhere(function($q) use ($proce_id){
+                    $q -> whereNull('maq_pro_id')
+                        ->where('proceso_id', '=', $proce_id);
+                })
+                ->count('valor');
+
             //variablesde operaciones
             $fs += $suma;
             $fc += $cuenta;
+
+            $data = ['proceso_id' => $proce_id, 'suma' => $fs, 'turno_id' => null, 'cantidad' => $cuenta, 'partida' => 'N/A', 'norma' => null, 'clave_id' => null, 'per_carga' => $usuario->id];
             //print($fs.' | '.$cuenta.' / ');
         }
-        if ($fc > 0) {
-            echo $val->nompro.' | '.$fs.' | '.$fc.'  fin suma dia || ';
-            $this->gua_act($fechas, $val, $fs, $fc);
-        }
+        //echo $data['proceso_id'].' | '.$data['suma'].' | '.$data['cantidad'].'  fin suma dia || ';
+        $this->gua_act($fechas, $data);
 
         return 'sm_d';
     }
 
     //operacion suma dia clave
-    public function sm_dc($val, $fechas){
+    public function sm_dc($val, $fechas, $usuario){
         //recorrido de formulas
         foreach ($val->formulas as $value) {
             $proce_id = $value->proceso_id;
             $maq_id = $value->maq_pros_id;
             $claves = carga::where('maq_pro_id', '=', $maq_id)
             ->whereBetween('fecha', [$fechas['hoy'], $fechas['mañana']])
-            ->with([
-                'clave' => function($cla){
-                    $cla ->withTrashed()
-                    -> select('id', 'CVE_ART', 'DESCR');
-                }
-            ])
             ->distinct()
-            ->get(['clave_id']);
+            ->get(['norma','clave_id']);
             $fs = 0;
             $fc = 0;
             foreach ($claves as $cl) {
@@ -160,16 +190,18 @@ class CalculosController extends Controller
                 //resultado
                 $fs += $suma;
                 $fc += $cuenta;
-                print($val->nompro.' | '.$cl->clave->CVE_ART.' | '.$suma.' | '.$cuenta.' - ');
+                //print($val->nompro.' | '.$cl->clave->CVE_ART.' | '.$suma.' | '.$cuenta.' - ');
+                $data = ['proceso_id' => $proce_id, 'suma' => $fs, 'turno_id' => null, 'cantidad' => $cuenta, 'partida' => 'N/A', 'norma' => $cl->norma, 'clave_id' => $cl->clave_id, 'per_carga' => $usuario->id];
+                $this->gua_act($fechas, $data);
             }
         }
-        echo ' fin suma clave dia || ';
+        //echo ' fin suma clave dia || ';
         return 'sm_dc';
 
     }
 
     //operacion suma turno
-    public function sm_t($val, $dep, $fechas){
+    public function sm_t($val, $dep, $fechas, $usuario){
         //se consultan los turnos que existe menos el turno vacio
         $turnos = turnos::where('departamento_id', '=', $dep)
             ->where('nomtur', '!=', 'Vacío')
@@ -213,16 +245,19 @@ class CalculosController extends Controller
                 //resultado
                 $fs += $suma;
                 $fc += $cuenta;
-                print( $val->nompro.' | '.$tur->nomtur.' | '.$fs.' | '.$fc.' / ');
+                //print( $val->nompro.' | '.$tur->nomtur.' | '.$fs.' | '.$fc.' / ');
+                $data = ['proceso_id' => $proce_id, 'suma' => $fs, 'turno_id' => $tur->id, 'cantidad' => $cuenta, 'partida' => 'N/A', 'norma' => null, 'clave_id' => null, 'per_carga' => $usuario->id];
+                $this->gua_act($fechas, $data);
+
             }
             //echo $tur;
         }
-        echo 'Fin suma turno || ';
+        //echo 'Fin suma turno || ';
         return 'sm_t';
     }
 
     //operacion suma turno clave
-    public function sm_tc($val, $dep, $fechas){
+    public function sm_tc($val, $dep, $fechas, $usuario){
         //se consultan los turnos que existe menos el turno vacio
         $turnos = turnos::where('departamento_id', '=', $dep)
             ->where('nomtur', '!=', 'Vacío')
@@ -280,14 +315,16 @@ class CalculosController extends Controller
                     //resultado
                     $fs += $suma;
                     $fc += $cuenta;
-                    if ($fc != 0) {
+                    $data = ['proceso_id' => $proce_id, 'suma' => $fs, 'turno_id' => $tur->id, 'cantidad' => $cuenta, 'partida' => 'N/A', 'norma' => $cl->norma, 'clave_id' => $cl->clave_id, 'per_carga' => $usuario->id];
+                    $this->gua_act($fechas, $data);
+                    /* if ($fc != 0) {
                         print($val->nompro.' | '.$tur->nomtur.' | '.$cl->clave->CVE_ART.' | '.$fs.' | '.$fc.' - ');
 
-                    }
+                    } */
                 }
             }
         }
-        echo 'fin de suma turno por clave';
+        //echo 'fin de suma turno por clave';
         return 'sm_tc';
     }
 }
