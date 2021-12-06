bed414cfaf874c055041b8d25abbaf23d6e47567
Usir95 <Usir95@gmail.com>
2021-12-06 17:28:39 -0600

    boton subir

--- resources/js/Pages/Produccion/Carga.vue
+++ resources/js/Pages/Produccion/Carga.vue
@@ -363,28 +363,28 @@
                         <td class="">
                             <div class="columnaIconos">
                                 <!-- nota operador -->
-                                <div class="iconoDetails tw-cursor-pointer" @click="notaCA(ca)" v-show="usuario.dep_pers.length != 0 & ((noCor == 'lid' | noCor == 'ope') & ca.equipo_id != null & ca.notaPen == 1)">
+                                <a class="iconoDetails tw-cursor-pointer" href="#agPer" @click="notaCA(ca)" v-show="usuario.dep_pers.length != 0 & ((noCor == 'lid' | noCor == 'ope') & ca.equipo_id != null & ca.notaPen == 1)">
                                     <span tooltip="Agregar nota" flow="left">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                         </svg>
                                     </span>
-                                </div>
+                                </a>
                                 <!-- nota objetivos -->
-                                <div class="iconoDetails tw-cursor-pointer" @click="notaOB(ca)" v-show="usuario.dep_pers.length != 0 & ((noCor == 'cor' | noCor == 'enc') & ca.proceso.tipo == 2 & ca.notaPen == 1)">
+                                <a class="iconoDetails tw-cursor-pointer" href="#agPer" @click="notaOB(ca)" v-show="usuario.dep_pers.length != 0 & ((noCor == 'cor' | noCor == 'enc') & ca.proceso.tipo == 2 & ca.notaPen == 1)">
                                     <span tooltip="Agregar nota" flow="left">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                         </svg>
                                     </span>
-                                </div>
-                                <div class="iconoEdit tw-cursor-pointer" @click="editCA(ca)" v-show="usuario.dep_pers.length == 0 | (ca.proceso.tipo != 2 & (noCor == 'cor' | noCor == 'enc'))">
+                                </a>
+                                <a class="iconoEdit tw-cursor-pointer" href="#agPer" @click="editCA(ca)" v-show="usuario.dep_pers.length == 0 | (ca.proceso.tipo != 2 & (noCor == 'cor' | noCor == 'enc'))">
                                     <span tooltip="Editar" flow="left">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                         </svg>
                                     </span>
-                                </div>
+                                </a>
                             </div>
                         </td>
                     </tr>
@@ -628,7 +628,8 @@
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <!-- Body -->
-            <div class="offcanvas-body small">
+            <div class="offcanvas-body small" >
+                <div id="Fobje"></div>
                 <!----------------------------- Formulario para coordinador ------------------------------------------->
                 <div class="m-5 tw-p-6 tw-bg-blue-600 tw-rounded-3xl tw-shadow-xl">
                     <div class="tw-mb-6 lg:tw-flex">
@@ -726,13 +727,13 @@
                                 <td class="tw-text-center">
                                     <div class="columnaIconos">
                                         <!-- editar objetivos -->
-                                        <div class="iconoEdit" @click="editOB(obje)" href="#Fobje">
+                                        <a class="iconoEdit" @click="editOB(obje)" href="#Fobje">
                                             <span tooltip="Editar" flow="left">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                                 </svg>
                                             </span>
-                                        </div>
+                                        </a>
                                         <div class="iconoDelete" @click="deleteOB(obje)">
                                             <span tooltip="Eliminar" flow="left">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
