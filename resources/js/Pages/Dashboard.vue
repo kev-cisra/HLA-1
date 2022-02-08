7adbee3804975959e37209c68f92325b2e84bed4
Usir95 <Usir95@gmail.com>
2022-02-08 10:07:14 -0600

    Cambio en empresas Login

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -25,6 +25,30 @@
                     </div>
                 </Link>
             </div>
+
+            <div class="tw-w-1/6 tw-p-0 tw-mt-2 tw-text-center tw-transition tw-duration-700 tw-ease-in-out tw-transform tw-h-1/6 hover:tw--translate-y-1 hover:tw-scale-110">
+                <Link href="/RecursosHumanos/Vacaciones" class="tw-no-underline icono">
+                    <div class="tw-flex tw-justify-center tw-mb-4">
+                        <i class="fas fa-calendar-check"></i>
+                    </div>
+                    <div>
+                        <h4 class="tw-text-base tw-font-semibold tw-text-gray-400 tw-uppercase">VACACIONES</h4>
+                        <span class="linea"></span>
+                    </div>
+                </Link>
+            </div>
+
+            <div class="tw-w-1/6 tw-p-0 tw-mt-2 tw-text-center tw-transition tw-duration-700 tw-ease-in-out tw-transform tw-h-1/6 hover:tw--translate-y-1 hover:tw-scale-110">
+                <Link href="/RecursosHumanos/Incidencias" class="tw-no-underline icono">
+                    <div class="tw-flex tw-justify-center tw-mb-4">
+                        <i class="fas fa-user-plus"></i>
+                    </div>
+                    <div>
+                        <h4 class="tw-text-base tw-font-semibold tw-text-gray-400 tw-uppercase">INCIDENCIAS</h4>
+                        <span class="linea"></span>
+                    </div>
+                </Link>
+            </div>
             <!-- Fin for -->
 
             <!-- Produccion -->
