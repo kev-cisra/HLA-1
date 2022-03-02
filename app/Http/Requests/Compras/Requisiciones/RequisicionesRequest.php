047b5ef9d6c2bd53a135c9677faeee1019c1303b
Usir95 <Usir95@gmail.com>
2022-03-02 13:53:05 -0600

    Correcion vistas y Verificacion de Partidas

--- app/Http/Requests/Compras/Requisiciones/RequisicionesRequest.php
+++ app/Http/Requests/Compras/Requisiciones/RequisicionesRequest.php
@@ -5,8 +5,7 @@ namespace App\Http\Requests\Compras\Requisiciones;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class RequisicionesRequest extends FormRequest
-{
+class RequisicionesRequest extends FormRequest{
 
     public function authorize(){
         return true;
@@ -16,16 +15,20 @@ class RequisicionesRequest extends FormRequest
         return [
             'IdUser' => ['required'],
             'IdEmp' => ['required'],
-            'Fecha' => ['required'],
             'Departamento_id' => ['required'],
             'NumReq' => ['numeric','required','digits_between:4,7',Rule::unique('requisiciones')->ignore($this->id)],
             'Codigo' => ['required'],
+            'Fecha' => ['required'],
             'Maquina' => ['required'],
             'Marca' => ['required'],
             'Tipo' => ['required'],
             'Nombre' => ['required'],
             'Marca' => ['required'],
             'Observaciones' => ['required'],
+            'Partida' => ['required'],
+            'Partida.*.Cantidad' => ['required'],
+            'Partida.*.Unidad' => ['required'],
+            'Partida.*.Descripcion' => ['required'],
         ];
     }
 
@@ -37,6 +40,9 @@ class RequisicionesRequest extends FormRequest
             'NumReq.digits_between' => 'El Número de requisición debe estar compuesto por 4 o 5 digitos',
             'NumReq.required' => 'El número de requisición es obligatorio',
             'NumReq.required' => 'El número de requisición ya ha sido registrado con Anterioridad',
+            'Partida.*.Cantidad' => [
+                'required' => 'Cantidad Requerida',
+            ],
         ];
     }
 
