54c946db3e40f5a155c4532e72e2c1f472a281af
Osiris <usir95.hp@gmail.com>
2022-04-19 18:02:26 -0500

    Añadir mas partidas a sol de insumos creadas

--- database/migrations/2022_03_04_140345_create_cotizaciones_sistemas_table.php
+++ database/migrations/2022_03_04_140345_create_cotizaciones_sistemas_table.php
@@ -18,10 +18,15 @@ class CreateCotizacionesSistemasTable extends Migration
             $table->unsignedBigInteger('IdUser'); //Id de Session
             $table->string('TipoPago',15);
             $table->string('Moneda',4)->default('MXN');
-            $table->float('TipoCambio')->default(1);
+            $table->string('TipoCambio',8,2)->default(1);
+            $table->string('CostoExtra',10,2)->nullable();
             $table->tinyInteger('Aprobado')->default(0);
             $table->string('Comentario')->default('S/C');
             $table->string('Archivo')->nullable();
+
+            $table->unsignedBigInteger('Proveedor_Sistemas_id')->nullable();
+            $table->foreign('Proveedor_Sistemas_id')->references("id")->on("proveedores_sistemas")
+            ->onUpdate("cascade");
 
             $table->unsignedBigInteger('requisicion_sistemas_id')->nullable();
             $table->foreign("requisicion_sistemas_id")->references("id")->on("requisiciones_sistemas")
